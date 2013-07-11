217a8324f1dc55e7c7bf8a1a0cbdbdc05f565ad4
Aidan Follestad <drummer.aidan@gmail.com>
2013-07-10 21:29:39 -0500

    Removed unnecessary folder.

--- gen/com/afollestad/silk/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package com.afollestad.silk;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
