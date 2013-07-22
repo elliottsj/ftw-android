75fe0bc555bf8f84df5b4a3b5b301a2ff4cd39ce
Aidan Follestad <drummer.aidan@gmail.com>
2013-07-22 12:22:59 -0500

    Fixes.

--- src/com/afollestad/silk/views/SilkDatePicker.java
+++ src/com/afollestad/silk/views/SilkDatePicker.java
@@ -2,6 +2,7 @@ package com.afollestad.silk.views;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -45,6 +46,9 @@ public class SilkDatePicker extends LinearLayout {
 
     private Calendar mCal;
     private int mCurrentYear;
+    private int lastMonth = -1;
+    private int lastDay = -1;
+    private int lastYear = -1;
     private SilkSpinnerAdapter mMonth;
     private SilkSpinnerAdapter mDay;
     private SilkSpinnerAdapter mYear;
@@ -85,15 +89,14 @@ public class SilkDatePicker extends LinearLayout {
         fillMonths();
         fillDays();
         fillYears();
-        monthSpinner.setSelection(mCal.get(Calendar.MONTH));
-        daySpinner.setSelection(mCal.get(Calendar.DAY_OF_MONTH) - 1);
-        yearSpinner.setSelection(mCurrentYear - getMinYear());
 
         monthSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (lastMonth == position) return;
                 mCal.set(Calendar.MONTH, position);
                 fillDays();
+                lastMonth = position;
             }
 
             @Override
@@ -103,7 +106,9 @@ public class SilkDatePicker extends LinearLayout {
         daySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (lastDay == position) return;
                 mCal.set(Calendar.DAY_OF_MONTH, position + 1);
+                lastDay = position;
             }
 
             @Override
@@ -113,13 +118,19 @@ public class SilkDatePicker extends LinearLayout {
         yearSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (lastYear == position) return;
                 mCal.set(Calendar.YEAR, getMinYear() + position);
+                lastYear = position;
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
+
+        monthSpinner.setSelection(mCal.get(Calendar.MONTH));
+        daySpinner.setSelection(mCal.get(Calendar.DAY_OF_MONTH) - 1);
+        yearSpinner.setSelection(mCurrentYear - getMinYear());
     }
 
     private void fillMonths() {
@@ -131,6 +142,7 @@ public class SilkDatePicker extends LinearLayout {
 
     private void fillDays() {
         int daysInMonth = mCal.getActualMaximum(Calendar.DAY_OF_MONTH);
+        if (daysInMonth == mDay.getCount()) return;
         mDay.clear();
         for (int i = 1; i <= daysInMonth; i++) mDay.add(i + "");
         mDay.notifyDataSetChanged();
