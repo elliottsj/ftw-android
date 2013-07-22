7b024576cf99062b161d7cbdcd6e6277573422bf
Aidan Follestad <drummer.aidan@gmail.com>
2013-07-22 10:35:19 -0500

    Log output fix.

--- src/com/afollestad/silk/cache/SilkCacheManager.java
+++ src/com/afollestad/silk/cache/SilkCacheManager.java
@@ -154,7 +154,7 @@ public final class SilkCacheManager<T extends SilkComparable> {
     public void write(List<T> items, boolean append) throws Exception {
         if (items == null || items.size() == 0) {
             if (cacheFile.exists() && !append) {
-                log("Adapter for " + cacheFile.getName() + " is empty, deleting file...");
+                log(cacheFile.getName() + " is empty, deleting file...");
                 cacheFile.delete();
             }
             return;
