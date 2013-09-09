461f93d5d475dbb194d0315d7fbd55bdd5c271f2
Aidan Follestad <drummer.aidan@gmail.com>
2013-09-08 21:32:20 -0500

    Fixes

--- library/src/com/afollestad/silk/caching/SilkCache.java
+++ library/src/com/afollestad/silk/caching/SilkCache.java
@@ -87,6 +87,8 @@ public class SilkCache<ItemType extends SilkComparable<ItemType>> extends SilkCa
     }
 
     public final SilkCache<ItemType> set(List<ItemType> items) {
+        if (items == null || items.size() == 0)
+            throw new IllegalArgumentException("You cannot set a null or empty collection.");
         getBuffer().clear();
         getBuffer().addAll(items);
         log("Cache overwritten with " + items.size() + " items");
