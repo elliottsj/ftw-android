ce2b5ba1e9788aa16015d73b2c47080efd7a4a46
Aidan Follestad <drummer.aidan@gmail.com>
2013-09-29 12:15:32 -0500

    Made configurable settings alpha-numeric

--- library/src/com/afollestad/silk/fragments/SilkFeedFragment.java
+++ library/src/com/afollestad/silk/fragments/SilkFeedFragment.java
@@ -1,6 +1,7 @@
 package com.afollestad.silk.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import com.afollestad.silk.Silk;
 import com.afollestad.silk.caching.SilkComparable;
@@ -34,8 +35,10 @@ public abstract class SilkFeedFragment<ItemType extends SilkComparable> extends 
 
     protected List<ItemType> onUpdateItems(List<ItemType> results, boolean paginated) {
         List<ItemType> items = getAdapter().getItems();
+        Log.d("HomeFragment", "Adapter size: " + items.size());
         if (results != null) {
             if (paginated) {
+                Log.d("HomeFragment", "Adapter size: " + items.size());
                 items.addAll(results);
             } else {
                 items = results;
