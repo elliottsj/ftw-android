be5894e34f13af00f7c88036803797175d71ee4f
Aidan Follestad <drummer.aidan@gmail.com>
2013-11-10 01:41:39 -0600

    Fixes

--- library/src/com/afollestad/silk/adapters/SilkCursorAdapter.java
+++ library/src/com/afollestad/silk/adapters/SilkCursorAdapter.java
@@ -50,7 +50,7 @@ public abstract class SilkCursorAdapter<ItemType extends SilkCursorItem> extends
             Method m = mClass.getDeclaredMethod("convert", Cursor.class);
             return (ItemType) m.invoke(o, getCursor());
         } catch (Exception e) {
-            throw new RuntimeException("An error occurred while invoking convert() of class " + mClass.getName());
+            throw new RuntimeException("An error occurred while invoking convert() of class " + mClass.getName() + ": " + e.getMessage());
         }
     }
 }
