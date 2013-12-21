7103a4ef60be76de307584ecaaf8685ec1c27142
Aidan Follestad <drummer.aidan@gmail.com>
2013-12-20 22:42:51 -0600

    Major updates. Switched to the navigation drawer and fixed some styling stuff for Android 4.4 KitKat.

--- library/src/com/afollestad/silk/activities/SilkDrawerActivity.java
+++ library/src/com/afollestad/silk/activities/SilkDrawerActivity.java
@@ -93,7 +93,7 @@ public abstract class SilkDrawerActivity extends Activity {
         invalidateOptionsMenu();
     }
 
-    private void setupDrawer() {
+    protected void setupDrawer() {
         mTitle = getTitle();
         DrawerLayout mDrawerLayout = getDrawerLayout();
         if (mDrawerLayout == null)
