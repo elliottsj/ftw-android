a7d80c9e914ff8c8db8090fdf615e5e6bcc8b0fa
Aidan Follestad <drummer.aidan@gmail.com>
2013-12-21 20:20:18 -0600

    Updates.

---
+++ library/src/com/afollestad/cardsui/CardCompressed.java
@@ -0,0 +1,29 @@
+package com.afollestad.cardsui;
+
+import android.content.Context;
+
+/**
+ * A vertically compressed Card with no thumbnail. The title is displayed on the left, the content on the right.
+ *
+ * @author Aidan Follestad (afollestad)
+ */
+public class CardCompressed extends Card {
+
+    public CardCompressed(String title, String content) {
+        super(title, content);
+    }
+
+    public CardCompressed(Context context, int title, int subtitle) {
+        super(context, title, subtitle);
+    }
+
+    @Override
+    public int getLayout() {
+        return R.layout.list_item_header_centered;
+    }
+
+    @Override
+    public boolean isClickable() {
+        return false;
+    }
+}
