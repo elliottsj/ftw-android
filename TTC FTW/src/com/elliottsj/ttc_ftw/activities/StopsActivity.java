f8b218c432e112eeab4abc0febfee7df9030af23
Spencer Elliott <me@elliottsj.com>
2013-12-22 21:24:05 -0500

    Remove PagerSlidingTabStrip

--- TTC FTW/src/com/elliottsj/ttc_ftw/activities/StopsActivity.java
+++ TTC FTW/src/com/elliottsj/ttc_ftw/activities/StopsActivity.java
@@ -4,19 +4,32 @@ package com.elliottsj.ttc_ftw.activities;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import com.astuetz.viewpager.extensions.PagerSlidingTabStrip;
 import com.elliottsj.ttc_ftw.R;
-import net.sf.nextbus.publicxmlfeed.impl.SimplestNextbusServiceAdapter;
 
 public class StopsActivity extends Activity {
 
     PagerSlidingTabStrip mTabs;
     ViewPager mViewPager;
     StopsTabsAdapter mStopsTabsAdapter;
+
+    private static final String TAG = "StopsActivity";
+
+    /**
+     * Returns true iff a network connection is available.
+     */
+    public boolean isNetworkConnected() {
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +42,6 @@ public class StopsActivity extends Activity {
         mStopsTabsAdapter = new StopsTabsAdapter(getFragmentManager());
         mViewPager.setAdapter(mStopsTabsAdapter);
         mTabs.setViewPager(mViewPager);
-
-        SimplestNextbusServiceAdapter svc = new SimplestNextbusServiceAdapter();
-
     }
 
     @Override
---
+++ TTC FTW/src/com/elliottsj/ttc_ftw/utilities/AndroidRPCImpl.java
@@ -0,0 +1,162 @@
+package com.elliottsj.ttc_ftw.utilities;
+
+import android.net.http.HttpResponseCache;
+import android.util.Log;
+import net.sf.nextbus.publicxmlfeed.impl.NextbusService;
+import net.sf.nextbus.publicxmlfeed.impl.RPCImpl;
+import net.sf.nextbus.publicxmlfeed.impl.RPCRequest;
+import net.sf.nextbus.publicxmlfeed.service.ServiceConfigurationException;
+import net.sf.nextbus.publicxmlfeed.service.ServiceException;
+import net.sf.nextbus.publicxmlfeed.service.TransientServiceException;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ */
+public class AndroidRPCImpl implements RPCImpl {
+
+    private static final Logger logger = Logger.getLogger(NextbusService.class.getName());
+
+    private long lastSuccessfulCallTimeUTC;
+    private long totalRPCCalls;
+    private long bytesReceived;
+
+    private static final String TAG = "AndroidRPCImpl";
+
+    // Enforces advisory warnings on bandwidth use - NextBus spec says 2MB/20sec Max
+    private static final long BANDWIDTH_LIMIT_INTERVAL_MILLISECONDS = 20*1000;  // 20 seconds
+    private static final long BANDWIDTH_LIMIT_INTERVAL_BYTES = 2^21;              // 2 megabytes
+
+    // State machine values for Sliding bandwidth monitor
+    private long bwLimitIntervalStartTime;
+    private long bwLimitIntervalStartBytes;
+
+    public AndroidRPCImpl() {
+    }
+
+    public String call(RPCRequest request) throws ServiceException {
+        HttpURLConnection c = null;
+
+        BufferedReader rd;
+        StringBuilder sb;
+        InputStream is;
+        String line;
+        try {
+            URL url = new URL(request.getFullHttpRequest());
+
+            c = (HttpURLConnection) url.openConnection();
+            Log.i(TAG, "RPC handler opened HTTP connection");
+
+            try {
+                HttpResponseCache cache = HttpResponseCache.getInstalled();
+                String response;
+                try {
+                    long maxSize = cache.maxSize();
+                    Map<String, List<String>> requestHeaders = new HashMap<String, List<String>>();
+//                    requestHeaders.put("Accept-Encoding", Arrays.asList("gzip", "deflate"));
+//                    requestHeaders.put("Accept", Arrays.asList("*/*"));
+//                    requestHeaders.put("Accept-Language", Arrays.asList("en-us"));
+                    CacheResponse cr = cache.get(url.toURI(), "GET", requestHeaders);
+                    if (cr != null) response = cr.toString();
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                }
+
+                c.setRequestProperty("Cache-Control", "only-if-cached");
+                is = new BufferedInputStream(c.getInputStream());
+            } catch (FileNotFoundException e) {
+                c.disconnect();
+
+                c = (HttpURLConnection) url.openConnection();
+                Log.i(TAG, "RPC handler opened HTTP connection");
+
+                Map<String, List<String>> requestHeaders = c.getRequestProperties();
+
+                c.connect();
+                int http_status = c.getResponseCode();
+                if (http_status != HttpURLConnection.HTTP_OK) {
+                    String msg = String.format("Received HTTP Status Code %s : %s", http_status, c.getResponseMessage());
+                    Log.w(TAG, msg);
+                    totalRPCCalls++;
+                    throw new TransientServiceException(msg);
+                }
+
+                is = new BufferedInputStream(c.getInputStream());
+            }
+
+
+            /* Read in the HTTP Response until end of buffer */
+            rd = new BufferedReader(new InputStreamReader(is));
+            sb = new StringBuilder();
+            while ((line = rd.readLine()) != null) {
+                bytesReceived += line.length();
+                sb.append(line);
+                logger.log(Level.FINEST, "Read " + line.length() + " bytes from the HTTP input buffer");
+//                Log.i(TAG, "Read " + line.length() + " bytes from the HTTP input buffer");
+                checkBandwidthLimits();
+            }
+
+            /* Done! Cleanup and return to the caller */
+            is.close();
+            rd.close();
+            Log.i(TAG, "RPC handler closed HTTP connection");
+            lastSuccessfulCallTimeUTC = System.currentTimeMillis();
+            return sb.toString();
+        } catch (MalformedURLException mfu) {
+            Log.e(TAG, "Invalid URL. Inspect: " + request.getFullHttpRequest(), mfu);
+            throw new ServiceConfigurationException(mfu);
+        } catch (IOException ioe) {
+            Log.w(TAG, "During http rpc to nextbus ", ioe);
+            throw new TransientServiceException(ioe);
+        } finally {
+            if (c != null) c.disconnect();
+            totalRPCCalls++;
+        }
+    }
+
+    /**
+     * Diagnostic method
+     * @return the last timestamp (in UTC time) when a Successful RPC was made
+     */
+    public long getLastSuccessfulCallTimeUTC() {
+        return lastSuccessfulCallTimeUTC;
+    }
+
+    /**
+     * Diagnostic method
+     * @return total number of RPC calls (failed and successful) made
+     */
+    public long getTotalRPCCalls() {
+        return totalRPCCalls;
+    }
+
+    /**
+     * Implements a simple Bandwidth limit check.
+     * When b/w is exceeded, a WARN Advisory is posted to the logger.
+     * Tests whether more than N bytes are sent within an M second interval
+     */
+    private void checkBandwidthLimits() {
+        // Reset the observation time window if needed
+        if (bwLimitIntervalStartTime == 0 || (System.currentTimeMillis()-bwLimitIntervalStartBytes) > BANDWIDTH_LIMIT_INTERVAL_MILLISECONDS) {
+            bwLimitIntervalStartTime = System.currentTimeMillis();
+            bwLimitIntervalStartBytes += bytesReceived;
+        }
+
+        // Check the total byte count inside the observation window
+        long bytesRecvInLimitWindow = bytesReceived - bwLimitIntervalStartBytes;
+        if ((bytesRecvInLimitWindow ) > BANDWIDTH_LIMIT_INTERVAL_BYTES) {
+            // The bandwidth limit has been exceeded!
+            Log.w(TAG, "Bandwidth advisory limit exceeded by "+bytesRecvInLimitWindow+" bytes!");
+        }
+    }
+
+    public void activate() { }
+
+    public void passivate() { }
+
+}
